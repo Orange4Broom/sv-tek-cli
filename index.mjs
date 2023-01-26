5b5353f441f3f2caedc94e2d4a1948fc24cadab5
Jakub Trnčák <trncak@rtsoft.cz>
2023-01-26 12:39:21 +0100

    Make menu

--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
 import fetch from "node-fetch";
+import readline from "readline";
+import inquirer from "inquirer";
 
 const fetchData = async () => {
     const req = await fetch('https://svatkyapi.cz/api/day');
@@ -8,7 +10,52 @@ const fetchData = async () => {
 
 (async () => {
     const data = await fetchData();
-    console.log(`Dnešní datum: ${(data.dayNumber) + '.' + (data.monthNumber) + '.'}`);
-    console.log(`Den: ${(data.dayInWeek)}`);
-    console.log(`Svátek má: ${data.name}`);
+
+    const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    });   
+
+    let userName = "";
+    
+    const showMenu = () => {
+        inquirer
+        .prompt([{
+            name: 'menu',
+            type: 'list',
+            choices: ['[0]  Dnešní den', '[1]  Dnešní datum', '[2]  Kdo má dneska svátek?', '[3]  Vypsat vše', '[4]  Konec'],
+        }
+        ]
+        ).then((answer) => {
+            switch (answer.menu) {
+                case '[0]  Dnešní den':
+                    console.log(`Dneska je: ${data.dayInWeek}`);
+                    return showMenu();
+
+                case '[1]  Dnešní datum':
+                    console.log(`Dnešní datum: ${(data.dayNumber) + '.' + (data.monthNumber) + '.'}`);
+                    return showMenu();
+
+                case '[2]  Kdo má dneska svátek?':
+                    console.log(`Dneska má svátek ${data.name}`);
+                    return showMenu();
+
+                case '[3]  Vypsat vše':
+                    console.log(`Dneska je: ${data.dayInWeek}`);
+                    console.log(`Dnešní datum: ${(data.dayNumber) + '.' + (data.monthNumber) + '.'}`);
+                    console.log(`Dneska má svátek ${data.name}`);
+                    return showMenu();
+
+                case '[4]  Konec':
+                    console.log('Konec programu');
+                    process.exit();
+            }
+        })
+    }
+
+    showMenu();
+    
+
 })();
+
+
